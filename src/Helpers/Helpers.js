4a12127a1c6ddf910b41acaa15959262b7f321b9
nikhilupadhyay19 <nikhilupadhyay19@gmail.com>
2022-08-01 11:52:29 +0530

    2 files modified, 1 added

---
+++ src/Helpers/Helpers.js
@@ -0,0 +1,20 @@
+const timeout = (s) => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(`Request is taking too long... More than ${s} seconds...`);
+    }, s * 1000);
+  });
+};
+
+export const getJson = async (url, errMsg = 'Something went wrong...') => {
+  try {
+    const res = await Promise.race([fetch(url), timeout()]);
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error(`${errMsg} ${res.status} ${data.message}`);
+    }
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const HomePage = () => {
+  const [product_ID, setProduct_ID] = useState('');
+  const [product_DATA, setProduct_DATA] = useState([]);
+  const [product_LOADED, setProduct_LOADED] = useState(false);
+
+  useEffect(() => {
+    
+  });
+
   return (
     <div className="home-page" id="homePage">
       <p>HomePage...</p>
