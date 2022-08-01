4c176d65fe3398c329b78c4dc7b4a1e2d23d3c3d
nikhilupadhyay19 <nikhilupadhyay19@gmail.com>
2022-08-01 17:56:18 +0530

    1 file modified

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProductStore from './ProductStore/ProductStore';
+import ProductStore from '../ProductStore/ProductStore';
 
 const HomePage = () => {
   return <ProductStore />;
