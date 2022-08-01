42726700ad707b43256ac6427fb1d1ceebe3d53c
nikhilupadhyay19 <nikhilupadhyay19@gmail.com>
2022-08-01 13:19:04 +0530

    1 file modified, 1 added, 1 deleted

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import { fakeStoreApiUrl } from '../../Helpers/config';
+import { getJson } from '../../Helpers/customFn';
 
 const HomePage = () => {
-  const [productId, setProductId] = useState('');
+  const [productId, setProductId] = useState(1);
   const [productData, setProductData] = useState([]);
   const [isProductLoaded, setProductLoaded] = useState(false);
+  const [fakeStoreApiError, setFakeStoreApiError] = useState(null);
 
-  useEffect(() => {});
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await getJson(`${fakeStoreApiUrl}/productId`);
+        setProductData(data);
+        setProductLoaded(true);
+      } catch (err) {
+        setFakeStoreApiError(err);
+      }
+    })();
+  }, [productId]);
 
   return (
     <div className="home-page" id="homePage">
