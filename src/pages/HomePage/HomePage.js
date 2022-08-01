c93c3a68a7a6a38631443edcb62273b175f08d8c
nikhilupadhyay19 <nikhilupadhyay19@gmail.com>
2022-08-01 11:39:08 +0530

    3 files modified, 3 added

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="home-page" id="homePage">
+      <p>HomePage...</p>
+    </div>
+  );
+};
+
+export default HomePage;
