f2798c1c1d10889bb06d50ee7c7f8db5c315b783
nikhilupadhyay19 <nikhilupadhyay19@gmail.com>
2022-08-01 18:27:52 +0530

    1 file modified

--- src/pages/JsonPlaceholder/JsonPlaceholder.js
+++ src/pages/JsonPlaceholder/JsonPlaceholder.js
@@ -1,9 +1,28 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, ButtonGroup, Button } from 'reactstrap';
 import { jsonPlaceholderUrl } from '../../Helpers/config';
+import { getJson } from '../../Helpers/customFn';
 
 const JsonPlaceholder = () => {
-  const [param, setPram] = useState();
+  const [param, setPram] = useState('posts');
+  const [jsonPlaceholderData, setJsonPlaceholderData] = useState([]);
+  const [isJsonPlaceholderDataLoaded, setJsonPlaceholderDataLoaded] =
+    useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await getJson(`${jsonPlaceholderUrl}/${param}`);
+
+        setJsonPlaceholderData(data);
+        setJsonPlaceholderDataLoaded(true);
+      } catch (err) {
+        setError(err);
+      }
+    })();
+  });
+
   return (
     <div className="json-placeholder">
       <Container>
